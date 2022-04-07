8fe56bbaec5ec23e58c04887aa251f30e523faf0
Jon Keam <jpkeam@gmail.com>
2022-04-07 19:06:45 -0400

    add ocp serverless presentation

--- src/Serverless.js
+++ src/Serverless.js
@@ -3,14 +3,20 @@ import BasePage from './BasePage';
 
 export default function Serverless() {
   const cells = [{
+    title: 'OpenShift Serverless',
+    subtitle: 'A developer focused presentation on Knative, serverless concepts, OpenShift Serverless, and how it works under the covers',
+    videoUrl: 'https://jkeam-people.s3.amazonaws.com/ocp-serverless-intro.mov',
+    lastModifiedDate: 'April 7, 2022',
+    description: 'This presentation is especially geared towards developers on how to more easily use Kubernetes.  It specifically talks about Knative, OpenShift Serverless, and includes a demo on how to spin up applications in a way that is more natural for developers than dealing with a mountain of Kubernetes YAMLs.  The serverless paradigm as a whole is discussed while also touching on some good use cases.  There will also be a deep dive of Knative and how it works under the hood.'
+  },{
     title: 'Event Driven Architecture Introduction',
     subtitle: 'Introduction and benefits of going event driven',
     videoUrl: 'https://jkeam-people.s3.amazonaws.com/eda.mp4',
     lastModifiedDate: 'June 22, 2021',
     description: 'Advances in technology, such as microservices and containers, continue to transform how agencies operate. Event-driven architecture (EDA), a software architecture and model for application design, is quickly becoming a preferred method for capturing insights and communicating changes instantly for improved customer experience. This talk exploreses these topics and talks specifically how the USDA can use EDA to achieve a flexible system that can adapt to changes and make decisions in real-time.'
   }, {
-    title: 'Serverless Introduction',
-    subtitle: 'Introduction to Serverless concepts and exploration of how OpenShift enables Serverless architectures',
+    title: 'Serverless Fireside Chat',
+    subtitle: 'Conversation about Serverless and exploration of how OpenShift enables Serverless architectures',
     linkUrl: 'https://www.brighttalk.com/webcast/16623/432634/optimizing-serverless-and-openshift-with-knative',
     imageUrl: 'https://jkeam-people.s3.amazonaws.com/serverless.png',
     imageAltText: 'OpenShift Serverless',
