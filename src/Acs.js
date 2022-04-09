6510d65fbb606b4c23cc282f4277535a5fbe4191
Jon Keam <jpkeam@gmail.com>
2022-04-08 21:47:17 -0400

    add acs access control

--- src/Acs.js
+++ src/Acs.js
@@ -3,6 +3,12 @@ import BasePage from './BasePage';
 
 export default function Acs() {
   const cells = [{
+    title: 'Access Control',
+    subtitle: 'Limiting what users and groups can do with Advanced Cluster Security',
+    videoUrl: 'https://jkeam-people.s3.amazonaws.com/acs-access-control.mov',
+    lastModifiedDate: 'April 8, 2022',
+    description: 'Advanced Cluster Security is an extremely powerful Kubernetes based security platform.  Part of its capabilities is the ability to tightly control exactly what users and groups have access to.  Different personas can have access to the right pieces of information so they can do their job, but not too much as to adhere to the security principal of least privledge.  This video walks through an example of how to configure this.'
+  },{
     title: 'Image Scanning',
     subtitle: 'Use the Advanced Cluster Security scanning tool via command line to scan your images.',
     videoUrl: 'https://jkeam-people.s3.amazonaws.com/stackrox_scanning.mov',
