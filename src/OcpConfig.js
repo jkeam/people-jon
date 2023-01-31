f3a7b0e0e4007bf5aad33568dbb22a1fb8ad14b7
Jon Keam <jpkeam@gmail.com>
2023-01-30 22:09:52 -0500

    add project request template video

--- src/OcpConfig.js
+++ src/OcpConfig.js
@@ -8,6 +8,12 @@ export default function OcpConfig() {
     videoUrl: 'https://jkeam-people.s3.amazonaws.com/developer-perspective-add-page.mov',
     lastModifiedDate: 'Mar 29, 2022',
     description: 'By default there are many ways to deploy applications onto OpenShift.  As an OpenShift admin, you might want to limit these options.  This video explains how to do that.'
+  }, {
+    title: 'Project Request Template',
+    subtitle: 'Configure an EgressNetworkPolicy to automatically be created for every new project',
+    videoUrl: 'https://jkeam-people.s3.amazonaws.com/openshift-project-template-egress.mp4',
+    lastModifiedDate: 'Jan 30, 2023',
+    description: 'Having to remember to manually create things like Limit, Quotas, or NetworkPolicies for every new project can be a cumbersome task.  With OpenShift, we can use the Project Request Template to have OpenShift easily and automatically do that for us.'
   }];
   return <BasePage cells={cells} subtitle="Configuration" />;
 }
