2abf5b04f233bcf415c678f0e537de377baa5863
Jon Keam <jpkeam@gmail.com>
2023-02-17 15:30:45 -0500

    add OCP OAuth

--- src/OcpConfig.js
+++ src/OcpConfig.js
@@ -14,7 +14,12 @@ export default function OcpConfig() {
     videoUrl: 'https://jkeam-people.s3.amazonaws.com/openshift-project-template-egress.mp4',
     lastModifiedDate: 'Jan 30, 2023',
     description: 'Having to remember to manually create things like Limit, Quotas, or NetworkPolicies for every new project can be a cumbersome task.  With OpenShift, we can use the Project Request Template to have OpenShift easily and automatically do that for us.'
+  }, {
+    title: 'OpenShift OAuth',
+    subtitle: 'Using OpenShift OAuth to log into your custom app',
+    videoUrl: 'https://jkeam-people.s3.amazonaws.com/openshift-oauth.mp4',
+    lastModifiedDate: 'Feb 17, 2023',
+    description: 'If you have ever used "log in with GitHub/Facebook/Google" then you have used OAuth.  But now you can do that with OpenShift.  You will even get a token from the login that you can use to perform actions on behalf of that user.  This video will walk you though how to do that.'
   }];
   return <BasePage cells={cells} subtitle="Configuration" />;
 }
-
