4dee76a13506465b415d894fc9f20f482d722854
Jon Keam <jpkeam@gmail.com>
2023-04-07 22:44:43 -0400

    add sso custom claim in jwt

--- src/SSO.js
+++ src/SSO.js
@@ -3,6 +3,12 @@ import BasePage from './BasePage';
 
 export default function SSO() {
   const cells = [{
+    title: 'SSO Custom Claims in JWT',
+    subtitle: 'Using Keycloak to add custom claims to our JWT',
+    videoUrl: 'https://jkeam-people.s3.amazonaws.com/keycloak-jwt-custom-claims.mp4',
+    lastModifiedDate: 'April 7, 2023',
+    description: 'OpenID Connect (OIDC) is a powerful authentication and authorization mechanism that utilizes JSON Web Tokens (JWT).  In this demo, we see how we can use Keycloak to add our own custom claims/attributes into the token.'
+  }, {
     title: 'SSO Demo',
     subtitle: 'Uses external Identity Provider and External User Federated Database',
     videoUrl: 'https://jkeam-people.s3.amazonaws.com/keycloak_demo.mov',
@@ -17,4 +23,3 @@ export default function SSO() {
   }];
   return <BasePage cells={cells} subtitle="SSO" />;
 }
-
